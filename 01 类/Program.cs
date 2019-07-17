8a98201d4981db5850c144ab1608ba52cd0edf9b
Khan1992 <453345486@qq.com>
2019-07-17 08:56:29 +0800

    首次提交

--- 01 类/Program.cs
+++ 01 类/Program.cs
@@ -8,8 +8,14 @@ namespace _01_类
 {
     class Program
     {
+        static double r;
+        const double PI = 3.14;
         static void Main(string[] args)
         {
+            Console.WriteLine("请输入圆的半径：");
+            Program.r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("圆的面积为：" + PI * Math.Pow(r, 2));
+            Console.ReadLine();
         }
     }
 }
